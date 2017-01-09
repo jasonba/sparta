7db152557e1535a983abe72b486b7f9757549340
Jason Banham <jason.banham@gmail.com>
2017-01-09 17:52:59 +0000

    Corrected a bug that lead to erroneous graphs

--- payload/rwlatency.d
+++ payload/rwlatency.d
@@ -5,6 +5,44 @@
  *
  * Author: Adam Leventhal
  * Copyright 2014 Delphix
+ *
+ * NOTE: Code upto and including the io:::done was writtenby Adam Leventhal
+ *       In the original the tick-10sec was an END statement, to print information
+ *       once an interrupt had been generated.
+ *       The modification to use tick-10sec was changed by Jason.Banham@Nexenta.COM
+ *       (and badly) in the first implementation.
+ *       Within SPARTA the data was not automatically analysed and until this was
+ * 	 recently plotted, no real analysis had taken place with the data sampled
+ *       over any period of time.
+ *       After plotting, the graph would start high and gradually decline to almost
+ *       zero throughput, which was found over multiple samples from multiple systems,
+ *       eg:
+ *
+ *    45 +-+-++-+-+-++-+-+-++-+-+-++-+-+-++-+-+-++-+-+-++-+-+-++-+-+-++-+-+-++-+
+ *       +      # +        +       +        +        ++-------+-------+-------++
+ *    40 +-+    #                                     |read throughput *******-+
+ *       |      #                                     write throughput-#######+|
+ *    35 +-+    #                                                            +-+
+ *    30 +-+    #                                                            +-+
+ *       |      #                                                              |
+ *    25 +-+    #                                                            +-+
+ *       |      #                                                              |
+ *    20 +-+    #                                                            +-+
+ *       |      #                                                              |
+ *    15 +-+    *                                                            +-+
+ *    10 +-+    *#                                                           +-+
+ *       |      *#                                                             |
+ *     5 +-+    *#                                                           +-+
+ *       +      **#####    +       +        +        +        +       +        +
+ *     0 +-+-++-+***************************************************************
+ *     13/12    13/12    13/12   13/12    13/12    13/12    13/12   13/12    13/12
+ *     09:40    09:50    10:00   10:10    10:20    10:30    10:40   10:50    11:00
+ *
+ *       Something was clearly wrong and after careful studying of the code I realised
+ *       that the 'start' timestamp was static, thus during the normalisation, the
+ *       calculation of 'timestamp - start' was always getting bigger!
+ *       The fix was to reset start everytime we went through tick-10sec. 
+ *
  */
 
 #pragma D option quiet
@@ -49,5 +87,6 @@ tick-10sec
     trunc(@i);
     trunc(@b);
     trunc(@v);
+    start = timestamp;
 }
 
